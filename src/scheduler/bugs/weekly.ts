52073064ea051d8563387cc0ea25af672903f1ef
Cristopher <cristophernamchee12@gmail.com>
2025-11-25 21:50:03 +0700

    feat: send table

--- src/scheduler/bugs/weekly.ts
+++ src/scheduler/bugs/weekly.ts
@@ -22,7 +22,7 @@ export async function sendWeeklyBugReport(env: Env) {
   const prevSunday = new Date(today);
   prevSunday.setDate(today.getDate() - 6);
 
-  const blocks = [
+  const firstBlock = [
     {
       type: 'header',
       text: {
@@ -35,8 +35,438 @@ export async function sendWeeklyBugReport(env: Env) {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: `Below is the curated report of <https://github.com/GDP-ADMIN/glchat/issues|active bugs in GLChat> from *${formatDate(prevSunday)}* until *${formatDate(today)}*`,
+        text: `Below is the curated report of <https://github.com/GDP-ADMIN/glchat/issues|weekly bugs in GLChat> from *${formatDate(prevSunday)}* until *${formatDate(today)}*`,
       },
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `🐞 *Issue(s) Opened*`,
+      },
+    },
+    {
+      type: 'table',
+      rows: [
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: ' ',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Internal',
+                    style: { bold: true },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'External',
+                    style: { bold: true },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P0',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.open[0].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.open[0].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P1',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.open[1].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.open[1].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P2',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.open[2].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.open[2].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      ],
+    },
+  ];
+
+  const secondBlock = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `✨ *Issue(s) Closed*`,
+      },
+    },
+    {
+      type: 'table',
+      rows: [
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: ' ',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Internal',
+                    style: { bold: true },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'External',
+                    style: { bold: true },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P0',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.closed[0].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.closed[0].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P1',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.closed[1].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.closed[1].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'P2',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.closed[2].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.closed[2].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        [
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'CaE',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.internal.closed[3].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: weeklyStats?.data.external.closed[3].toString(),
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      ],
     },
   ];
 
@@ -48,7 +478,19 @@ export async function sendWeeklyBugReport(env: Env) {
     },
     body: JSON.stringify({
       channel: env.SLACK_CHANNEL,
-      blocks,
+      blocks: firstBlock,
+    }),
+  });
+
+  await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${env.SLACK_BOT_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      channel: env.SLACK_CHANNEL,
+      blocks: secondBlock,
     }),
   });
 }
