21efc67284178c6e22746fea5d24ef85f8cb9cd8
Cristopher <cristopher@gdplabs.id>
2026-01-29 11:36:12 +0700

    chore: temporarily remove DM reminder feature

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { sendMessageToChannel } from './scheduler/channel';
-import { sendMessageToPICs } from './scheduler/personal';
 
 import type { Env } from './types';
 
 const schedules: Record<string, (env: Env) => Promise<void>> = {
-  '0 5 * * 2-6': sendMessageToPICs,
+  // '0 5 * * 2-6': sendMessageToPICs,
   '30 8 * * 2-6': sendMessageToChannel,
 };
 
